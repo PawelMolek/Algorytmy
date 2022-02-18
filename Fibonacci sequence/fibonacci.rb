06308996218b262314d0a033dfa94b959d753096
PawelMolek <pawel.molek@gmail.com>
2022-02-18 18:46:29 +0100

    Fibonacci algorith faster version added

--- Fibonacci sequence/fibonacci.rb
+++ Fibonacci sequence/fibonacci.rb
@@ -1,3 +1,4 @@
+#recursive (slower) version
 def fibonacci (number)
   if number <= 1
     return number
@@ -10,3 +11,16 @@ puts "Enter number of Fibonacci sequence which you want to get"
 number = gets.chomp.to_i
 
 puts fibonacci(number)
+
+#fastest version
+# def fibonacci( n )
+#   i = 1
+#   j = 1
+#   while n > 1
+#     k = i + j
+#     i = j
+#     j = k
+#     n -= 1
+#   end
+#   return i
+# end
